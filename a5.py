0a83e9b19b17d5fbd8f115b7fbe69546421e8e86
bsh <dan990429@gmail.com>
2020-06-30 09:56:06 +0900

    add proj

---
+++ a5.py
@@ -0,0 +1,3 @@
+for f in range(0, 100, 10):
+    c = (f-32)*5/9
+    print(f, ' ', round(c, 2))
---
+++ a6.py
@@ -0,0 +1,9 @@
+a=input()
+
+i=0
+j=0
+while len(a)!=i:
+    j = j + int(a[i])
+    i += 1
+
+print(j)
