0b93bdfc82ac644d01f67e23cefe5992c4253e03
bsh <dan990429@gmail.com>
2020-07-06 13:28:43 +0900

    a

---
+++ a.py
@@ -0,0 +1,17 @@
+training_data = [["연두", 3, '사과'],
+                 ['노랑', 3, '사과'],
+                 ['빨강', 2, '포도'],
+                 ['빨강', 1, '포도'],
+                 ['노랑', 3, '레몬']
+                 ]
+
+def fruit_counts(data):
+    counts={}
+    for row in data:
+        label = row[-1]
+        if label not in counts:
+            counts[label]=0
+        counts[label]+=1
+    return counts
+result =fruit_counts(training_data)
+print(result)
