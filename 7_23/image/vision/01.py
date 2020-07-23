e06e948128312304446ed3a108b39410a3e31968
dan990429 <dan990429@naver.com>
2020-07-23 11:54:43 +0900

    Move file

--- 7_23/image/vision/01.py
+++ 7_23/image/vision/01.py
@@ -1,9 +1,11 @@
-# %% [markdown]
 import numpy as np
 import cv2
 
+# 위에서 아래순으로 덮어씀
+
 # Create a black image
-img = np.zeros((512, 512, 3), np.uint8)
+img = np.zeros((512, 512, 4), np.uint8)
+# img = cv2.imread("messi5.jpg", 1)
 # Draw a diagonal blue line with thickness of 5 px
 img = cv2.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
 
@@ -21,9 +23,17 @@ cv2.putText(img, "OpenCV", (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
 cv2.imshow("image", img)
 k = cv2.waitKey(0)
 
+print(img[100, 100])
+img[100, 101] = [255, 255, 255]
+img[99, 101] = [255, 255, 255]
+img[100:100, 200:200] = [255, 255, 255]
+
 
 if k == 27:  # wait for ESC key to exit
     cv2.destroyAllWindows()
 elif k == ord("s"):  # wait for 's' key to save and exit
     cv2.imwrite("messigray.png", img)
     cv2.destroyAllWindows()
+
+
+# %%
