d7ceca8f070166e931986623ab61c975149261fd
dan990429 <dan990429@naver.com>
2020-07-23 11:32:49 +0900

    7_23/02.py Fix errors while playing videos

--- 7_23/00.py
+++ 7_23/00.py
@@ -1,2 +1,15 @@
-aa = "aaaaaa"
-print(aa)
+# %% [markdown]
+import numpy as np
+import cv2
+
+
+img = cv2.imread("messi5.jpg", 0)
+cv2.imshow("image\\", img)
+k = cv2.waitKey(0)
+# %% [markdown]
+
+if k == 27:  # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord("s"):  # wait for 's' key to save and exit
+    cv2.imwrite("messigray.png", img)
+    cv2.destroyAllWindows()
---
+++ 7_23/02.py
@@ -0,0 +1,21 @@
+# %% [markdown]
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture("coin.avi")
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    # NOTE Add this while playing videos
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow("frame", gray)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
